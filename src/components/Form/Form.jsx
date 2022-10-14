f9ce80c45c82b2028f698459cf2d82cf65b1bb19
Leandroni1983 <leandronicolas1983@gmail.com>
2022-10-14 08:49:08 -0300

    first commit

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react'
+import { Input, Box, Flex, useColorMode, Button, VStack } from '@chakra-ui/react';
+import getSummonerData from '../RiotApi/RiotApi';
+function Form() {
+    const { colorMode } = useColorMode()
+    const inputEl = useRef(null)
+    const handleSubmit = () => {
+        getSummonerData(inputEl.current.value)
+    }
+
+    return (
+        <Flex direction={'column'} align={'center'} justify={'center'}>
+            <h1>ReactApiLol</h1>
+            <Box>
+                <form action="submit">
+                    <VStack m={1}>
+                        <Input
+                            ref={inputEl}
+                            size={'lg'}
+                            w={500}
+                            bg={'red.400'}
+                            borderRadius={25}
+                            placeholder={'Invocador'}
+                            _placeholder={{ color: 'black', opacity: 1 }} />
+                        <Button onClick={handleSubmit} bg={colorMode === 'light' ? 'blue.300' : 'black'}>buscar</Button>
+                    </VStack>
+                </form>
+            </Box>
+        </Flex>
+
+    )
+}
+
+export default Form
---
+++ src/components/RiotApi/RiotApi.js
@@ -0,0 +1,17 @@
+
+const API_KEY = 'RGAPI-42684f88-d81c-4293-8388-89295c2b6c47'
+
+
+const getSummonerData = (summonerName) => {
+    fetch(`https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${API_KEY}`)
+        .then(res => res.json())
+        .then(res => getSummonerEntries(res.id))
+
+}
+const getSummonerEntries = (id) => {
+    fetch(`https://la2.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${API_KEY}`)
+        .then(res => res.json())
+        .then(res => console.log(res))
+}
+
+export default getSummonerData 

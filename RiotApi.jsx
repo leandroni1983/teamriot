71ca0a7f6750983f05647fd3b127b5fc6d3c2e36
Leandroni1983 <leandronicolas1983@gmail.com>
2022-10-14 11:35:34 -0300

    first commit

--- RiotApi.jsx
+++ RiotApi.jsx
@@ -1,7 +1,5 @@
 
-//const API_KEY = 'RGAPI-42684f88-d81c-4293-8388-89295c2b6c47'
 const API_KEY = import.meta.env.VITE_KEY
-console.log(import.meta.env.VITE_KEY)
 const getSummonerData = (summonerName) => {
     fetch(`https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${API_KEY}`)
         .then(res => res.json())
